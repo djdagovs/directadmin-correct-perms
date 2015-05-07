ab6987ae7bfe841874b0d8b0e487493e9a7f535f
Richard Reijmers <rreijmers@denit.nl>
2015-05-07 22:12:25 +0200

    Feelin' confident

--- directadmin-correct-perms.sh
+++ directadmin-correct-perms.sh
@@ -32,24 +32,24 @@ for USER in ${USERS}; do
 		ECHOYELLOW ${DOMAIN}
 		if [[ -z ${EXEC} ]]; then
 			ECHORED "NOT EXECUTING"
-#			# Find all files without perm 0644
-#			find /home/${USER}/domains/${DOMAIN}/public_html -type f ! -perm 0644 -ls
-#			# Find all directories without perm 0755
-#			find /home/${USER}/domains/${DOMAIN}/public_html -type f ! -perm 0755 -ls
-#			# Find all files of directories without correct ownership
-#			find /home/${USER}/domains/${DOMAIN}/public_html ! -user ${USER} -ls
-#			# Find all files of directories without correct ownership
-#			find /home/${USER}/domains/${DOMAIN}/public_html ! -group ${USER} -ls
+			# Find all files without perm 0644
+			find /home/${USER}/domains/${DOMAIN}/public_html -type f ! -perm 0644 -ls
+			# Find all directories without perm 0755
+			find /home/${USER}/domains/${DOMAIN}/public_html -type d ! -perm 0755 -ls
+			# Find all files of directories without correct ownership
+			find /home/${USER}/domains/${DOMAIN}/public_html ! -user ${USER} -ls
+			# Find all files of directories without correct ownership
+			find /home/${USER}/domains/${DOMAIN}/public_html ! -group ${USER} -ls
 		else
 			ECHOBLUE "EXECUTING"
-#			# Find all files without perm 0644
-#			find /home/${USER}/domains/${DOMAIN}/public_html -type f ! -perm 0644 -ls
-#			# Find all directories without perm 0755
-#			find /home/${USER}/domains/${DOMAIN}/public_html -type f ! -perm 0755 -ls
-#			# Find all files of directories without correct ownership
-#			find /home/${USER}/domains/${DOMAIN}/public_html ! -user ${USER} -ls
-#			# Find all files of directories without correct ownership
-#			find /home/${USER}/domains/${DOMAIN}/public_html ! -group ${USER} -ls
+			# Find all files without perm 0644
+			find /home/${USER}/domains/${DOMAIN}/public_html -type f ! -perm 0644 -exec chmod -v 644 {} \;
+			# Find all directories without perm 0755
+			find /home/${USER}/domains/${DOMAIN}/public_html -type d ! -perm 0755 -exec chmod -v 755 {} \;
+			# Find all files of directories without correct ownership
+			find /home/${USER}/domains/${DOMAIN}/public_html ! -user ${USER} -exec chown -v ${USER}:${USER} {} \;
+			# Find all files of directories without correct ownership
+			find /home/${USER}/domains/${DOMAIN}/public_html ! -group ${USER} -exec chown -v ${USER}:${USER} {} \;
 		fi
 	done
 done
