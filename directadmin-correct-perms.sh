77513c2f693c0c4e33993f26b0c26b269c4861c2
Richard Reijmers <rreijmers@denit.nl>
2015-05-07 21:43:38 +0200

    Echo user

--- directadmin-correct-perms.sh
+++ directadmin-correct-perms.sh
@@ -5,6 +5,7 @@ USERS=$(ls /usr/local/directadmin/data/users)
 
 # Loop users and get domains
 for USER in ${USERS}; do
+	echo ${USER}
 	# List all virtualhosts for this user
 	cat /usr/local/directadmin/data/users/${USER}/domains.list
 done
