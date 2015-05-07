378ea8ecf756004b21d9767c7aa4528e9d2f2ac6
Richard Reijmers <rreijmers@denit.nl>
2015-05-07 21:45:02 +0200

    Fancy colors

--- directadmin-correct-perms.sh
+++ directadmin-correct-perms.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Some generic functions
+ECHORED()       {
+        echo -e "\e[1;31m${1}\e[0m"
+}
+
+ECHOYELLOW()    {
+        echo -e "\e[1;33m${1}\e[0m"
+}
+
+ECHOGREEN()     {
+        echo -e "\e[1;32m${1}\e[0m"
+}
+
+ECHOBLUE()      {
+        echo -e "\e[1;34m${1}\e[0m"
+}
+
 # List all virtualhosts
 USERS=$(ls /usr/local/directadmin/data/users)
 
